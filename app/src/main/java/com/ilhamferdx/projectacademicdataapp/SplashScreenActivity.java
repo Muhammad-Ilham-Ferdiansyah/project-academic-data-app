2802a07709ad92bd5a3ab7dc785e4415b7a9c6c6
Muhammad-Ilham-Ferdiansyah <ilhamferdiansyah737@gmail.com>
2022-01-15 10:40:38 +0700

    Fixed SplashScreenActivity

--- app/src/main/java/com/ilhamferdx/projectacademicdataapp/SplashScreenActivity.java
+++ app/src/main/java/com/ilhamferdx/projectacademicdataapp/SplashScreenActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -12,19 +13,17 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
 
-        int SPLASH_SCREEN_TIME = 3000; //3 detik
-
-        new Handler().postDelayed(new Runnable() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(com.ilhamferdx.projectacademicdataapp.SplashScreenActivity.this, LoginActivity.class);
-                com.ilhamferdx.projectacademicdataapp.SplashScreenActivity.this.startActivity(intent);
-                com.ilhamferdx.projectacademicdataapp.SplashScreenActivity.this.finish();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
             }
-        }, SPLASH_SCREEN_TIME);
+        }, 3000L); //3000 L = 3 detik
     }
 }

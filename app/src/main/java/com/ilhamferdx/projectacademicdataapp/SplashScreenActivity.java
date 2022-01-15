6d91e38f3f797699009acb1d48adfd39bb399f00
aldodwrzy <aldodwrzy@gmail.com>
2022-01-15 09:19:02 +0700

    Nambah SplashScreen

--- app/src/main/java/com/ilhamferdx/projectacademicdataapp/SplashScreenActivity.java
+++ app/src/main/java/com/ilhamferdx/projectacademicdataapp/SplashScreenActivity.java
@@ -2,7 +2,10 @@ package com.ilhamferdx.projectacademicdataapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        int SPLASH_SCREEN_TIME = 3000; //3 detik
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(com.ilhamferdx.projectacademicdataapp.SplashScreenActivity.this, LoginActivity.class);
+                com.ilhamferdx.projectacademicdataapp.SplashScreenActivity.this.startActivity(intent);
+                com.ilhamferdx.projectacademicdataapp.SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_SCREEN_TIME);
     }
 }
